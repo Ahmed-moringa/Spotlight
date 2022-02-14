f789f3fa43aaa389e942a610560e5e39a90cd683
Ahmed-moringa <ahmed.mohamed@student.moringaschool.com>
2022-02-14 13:46:27 +0300

    defined welcome view

--- spotlight/views.py
+++ spotlight/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+import datetime as dt
+from django.http import HttpResponse
 # Create your views here.
+def welcome(request):
+    return HttpResponse('Welcome to Spotlight')
