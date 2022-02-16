7f3a1cc6fe5db4340148404f8d6007d8ee8f7e53
Ahmed-moringa <ahmed.mohamed@student.moringaschool.com>
2022-02-16 15:16:59 +0300

    fixed error with styling

--- Spot/settings.py
+++ Spot/settings.py
@@ -23,8 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3pzxjqkj@b7eld^k9zz12)hy0hn_*mzwp$-yqfzsvw&t#fu!($'
-
+SECRET_KEY= config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
